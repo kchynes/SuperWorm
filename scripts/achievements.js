9e7804459d3bbbad4dc5c24f9ddf7d26523e1d87
johndrevniok <johndrevniok@gmail.com>
2015-05-22 18:42:57 -0400

    Fixes Issue #23
    Achievement message appears the moment the achievement is unlocked.(More
    testing will need to be done)
    Furthermore, I added a achievement message modal that appears. However,
    it could look nicer.

--- scripts/achievements.js
+++ scripts/achievements.js
@@ -1,8 +1,19 @@
 function incrementAchievementProgress(id, increment){
 	_ACHIEVEMENTS[id].Progress += increment;
+	checkAchievementProgress(id);
 }
 
 function updateAchievementProgress(id, updatedValue){
-	if(_ACHIEVEMENTS[id].Progress < updatedValue)
+	if(_ACHIEVEMENTS[id].Progress < updatedValue){
 		_ACHIEVEMENTS[id].Progress = updatedValue;
+		checkAchievementProgress(id);
+	}
+}
+
+function checkAchievementProgress(id){
+	if(_ACHIEVEMENTS[id].Progress == _ACHIEVEMENTS[id].Total){
+		$(".modal-body #AchievementMessageText").text( _ACHIEVEMENTS[id].Desc);
+		$('#AchievementMessage').modal();      
+	}
+	loadAchievements();
 }
